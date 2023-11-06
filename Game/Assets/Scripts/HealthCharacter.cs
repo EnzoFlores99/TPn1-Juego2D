225921b155250ef4217e53b9b7aae0fa46a2305f
EnzoFlores99 <143664695+EnzoFlores99@users.noreply.github.com>
2023-11-05 23:31:09 -0300

    daño y efecto agregada

--- Game/Assets/Scripts/HealthCharacter.cs
+++ Game/Assets/Scripts/HealthCharacter.cs
@@ -6,6 +6,9 @@ public class HealthCharacter : MonoBehaviour
 {
     public static HealthCharacter instance;
     public int currentHealth, maxHealth;
+    public float invincibleLenght;
+    private float invincibleCounter;
+    private SpriteRenderer theSr;
     private void Awake()
     {
         instance = this;
@@ -13,19 +16,39 @@ public class HealthCharacter : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        theSr= GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-        
+        if(invincibleCounter>0)
+        {
+            invincibleCounter -= Time.deltaTime;
+
+            if(invincibleCounter<= 0)
+            {
+                theSr.color = new Color(theSr.color.r, theSr.color.g, theSr.color.b,1f);
+            }
+        }
     }
 
     public void DealDamage()
     {
-        currentHealth--;
+        if(invincibleCounter<=0)
+        {
+            currentHealth--;
         if (currentHealth<=0)
         {
+            currentHealth=0;
             gameObject.SetActive(false);
         }
+        else
+        {
+            invincibleCounter = invincibleLenght;
+            theSr.color = new Color(theSr.color.r, theSr.color.g, theSr.color.b,0.5f);
+        }
+
         ControllerHeart.instance.UpdateHealthDisplay();
+        }
+        
     }
 }
